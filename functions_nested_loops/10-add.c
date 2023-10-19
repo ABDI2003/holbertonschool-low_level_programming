5c8e263dc10434867ba37dd338dbd675daed30ff
ABDI2003 <abdullin2003@gmail.com>
2023-10-19 00:25:14 -0700

    Write a function that prints all natural numbers from n to 98, followed by a new line

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Returns the sum of two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The sum of @a and @b
+ */
+
+int add(int a, int b)
+{
+	int num1 = a;
+	int num2 = b;
+
+	return (num1 + num2);
+}
